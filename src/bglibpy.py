159e478695ba77e9d4c6330cd285f36041b9fcc4
Werner Van Geit <werner.vangeit@epfl.ch>
2012-11-29 15:57:45 +0100

    Some changes to make bglibpy run on Linsrv2

--- src/bglibpy.py
+++ src/bglibpy.py
@@ -22,8 +22,7 @@ installdir = os.path.dirname(__file__)
 pathsconfig_filename = installdir+"/paths.config"
 
 if os.path.exists(pathsconfig_filename):
-    pathsconfig = {line.strip().split("=")[0]: line.strip().split("=")[1] for
-    line in open(pathsconfig_filename, "r")}
+    pathsconfig = dict((line.strip().split("=")[0], line.strip().split("=")[1]) for line in open(pathsconfig_filename, "r"))
 else:
     raise Exception("Sorry, can not find the file paths.config")
 
@@ -31,6 +30,8 @@ os.environ["HOC_LIBRARY_PATH"] = pathsconfig["HOC_LIBRARY_PATH"]
 
 sys.path = [pathsconfig["NRNPYTHONPATH"]]  + sys.path
 import neuron
+print "Imported neuron from %s" % neuron.__file__
+
 neuron.h.nrn_load_dll(pathsconfig["NRNMECH_PATH"])
 neuron.h.load_file("stdrun.hoc")
 
