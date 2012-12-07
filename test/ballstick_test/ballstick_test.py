d9eb90e22d0307de7e0e06069d7ffa410c07f7b1
Werner Van Geit <werner.vangeit@epfl.ch>
2012-12-07 16:13:29 +0100

    Update the ballstick morphology so that it doesn't contain an axon

--- test/ballstick_test/ballstick_test.py
+++ test/ballstick_test/ballstick_test.py
@@ -12,7 +12,9 @@ def main():
     print "Loaded", cell, "OK"
     print "Soma length %f, diameter %f, area %f" % (cell.soma.L, cell.soma.diam, bglibpy.neuron.h.area(0.5, sec=cell.soma))
     print "Dend length %f, diameter %f, area %f" % (cell.basal[0].L, cell.basal[0].diam, bglibpy.neuron.h.area(0.5, sec=cell.basal[0]))
-    print "Axon length %f, diameter %f, area %f" % (cell.axonal[0].L, cell.axonal[0].diam, bglibpy.neuron.h.area(0.5, sec=cell.axonal[0]))
+    #print "Axon length %f, diameter %f, area %f" % (cell.axonal[0].L, cell.axonal[0].diam, bglibpy.neuron.h.area(0.5, sec=cell.axonal[0]))
+    #print "Axon length %f, diameter %f, area %f" % (cell.axonal[1].L, cell.axonal[1].diam, bglibpy.neuron.h.area(0.5, sec=cell.axonal[1]))
+    bglibpy.neuron.h.topology()
 
 if __name__ == "__main__":
     main()
