d6e7a08942713116d21974d8290d50c99a316f02
Ben Torben-Nielsen <benjamin.torben-nielsen@epfl.ch>
2012-12-12 13:00:41 +0100

    Nose tests for the ball-and-stick model. Part I: comparison of B&S models with ExpSyn (requires Willems code for some tests)

--- test/ballstick_test/btn_bs.py
+++ test/ballstick_test/btn_bs.py
@@ -4,6 +4,7 @@ Models simulated in N disticnt ways, all shoudl
 """
 
 import numpy as np
+import numpy.testing
 import matplotlib.pyplot as plt
 
 import bglibpy
@@ -12,6 +13,10 @@ import bluepy
 import sys
 sys.path.append('/home/torben/sandbox/willem/DendriteApprox/')
 from greensFunctionCalculator import *
+
+'''Due to some path difficulties with BlueConfig, change directory'''
+import os
+os.chdir('test/ballstick_test')
 
 T_STOP = 100
 V_INIT = -75
@@ -24,26 +29,36 @@ SYN_DELAY = 0.025
 SYN_G = 0.001
 SYN_LOC = 1.0
 
+CM = -1
+RM = -1
+RA = -1
+EL = -1
+soma_L, soma_D, soma_A, dend0_L, dend0_D, dend0_A, dend0_NSEG = \
+  -1,-1,-1,-1,-1,-1,-1
+''' Actual configuration by calling compute_some_settings_from_ASC_file()
+'''
+
 surface = lambda r, h: 2*np.pi*r*h
 
-cell = bglibpy.Cell("ballstick.hoc", "./")
-soma_L,soma_D, soma_A = cell.soma.L, cell.soma.diam, bglibpy.neuron.h.area\
-  (0.5, sec=cell.soma)
-print 'SOMA L=%f, diam=%f,surf=%f' % (soma_L,soma_D,soma_A)
+def compute_some_settings_from_ASC_file() :
+    global CM,RM,RA,EL, \
+      soma_L, soma_D, soma_A, dend0_L, dend0_D, dend0_A,dend0_NSEG
+    cell = bglibpy.Cell("ballstick.hoc", "./")
+    soma_L,soma_D, soma_A = cell.soma.L, cell.soma.diam, bglibpy.neuron.h.area\
+      (0.5, sec=cell.soma)
+    print 'SOMA L=%f, diam=%f,surf=%f' % (soma_L,soma_D,soma_A)
 
-dend0_L,dend0_D,dend0_A  = cell.basal[0].L, cell.basal[0].diam, \
-  bglibpy.neuron.h.area(0.5, sec=cell.basal[0])
-print 'DENDRITE L=%f, diam=%f,surf=%f' % (dend0_L,dend0_D,dend0_A)
+    dend0_L,dend0_D,dend0_A  = cell.basal[0].L, cell.basal[0].diam, \
+      bglibpy.neuron.h.area(0.5, sec=cell.basal[0])
+    dend0_NSEG = cell.basal[0].nseg
+    print 'DENDRITE L=%f, diam=%f,surf=%f' % (dend0_L,dend0_D,dend0_A)
 
-''' I assume uniform passive properties shared by the soma and dendrites '''
-CM = cell.soma.cm
-RM = 1.0 / cell.soma(0.5).g_pas
-RA= cell.soma.Ra
-EL= cell.soma(0.5).e_pas
+    ''' I assume uniform passive properties shared by the soma and dendrites '''
+    CM = cell.soma.cm
+    RM = 1.0 / cell.soma(0.5).g_pas
+    RA= cell.soma.Ra
+    EL= cell.soma(0.5).e_pas
   
-''' N simulations of ... the same '''
-
-''' 1: PyNEURON (Hines) '''
 def run_hines_bs(soma_l,soma_d) :
     soma = bglibpy.neuron.h.Section()
     soma.L = soma_l
@@ -58,7 +73,7 @@ def run_hines_bs(soma_l,soma_d) :
     dend = bglibpy.neuron.h.Section()
     dend.L = dend0_L
     dend.diam = dend0_D
-    dend.nseg = cell.basal[0].nseg
+    dend.nseg = dend0_NSEG
     print 'rune_hines: dend.nseg=%f' % (dend.nseg) 
     dend.cm = CM
     dend.Ra = RA
@@ -92,92 +107,143 @@ def run_hines_bs(soma_l,soma_d) :
     hines_v = np.array(v_vec)
     hines_t = np.array(t_vec)
     return hines_t,hines_v
+
+def run_analytic(dt) :
+    ''' write config file for Willem '''
+    f_name = 'bs.cfg'
+    outF = open(f_name,'w')
+    outF.write('[neuron]\n')
+    outF.write('CM: %f\n' % (CM))
+    outF.write('RM: %f\n' % (RM))
+    outF.write('RA: %f\n' % (RA))
+    outF.write('EL: %f\n\n' % (EL))
+    outF.write('[soma]\n')
+    outF.write('D: ' + str(d_derived)+'\n')
+    outF.write('L: ' + str(l_derived)+'\n\n')
+    outF.write('[morph]\n')
+    outF.write('lengths: [%f]\n' % (dend0_L))
+    outF.write('diams: [%f]\n' % (dend0_D))
+    outF.close()
+
+    print 'going to do the analytical stuff'
+    v_willem,t_willem = compute_system([[SYN_ACTIVATION_T]],T_STOP,dt,\
+                                       conffile_name=f_name,numsyn=1,\
+                                       syndend=[0],synloc=[SYN_LOC],\
+                                       gbar=[SYN_G],decay=[SYN_DECAY],\
+                                       E_rev=[SYN_E])
+    return t_willem, v_willem
+
+def run_bglib_bs() :
+    cell = bglibpy.Cell("ballstick.hoc", "./")
+    syn = bglibpy.neuron.h.ExpSyn(SYN_LOC,sec=cell.basal[0])
+    syn.tau = SYN_DECAY
+    syn.e = SYN_E
+    ns = bglibpy.neuron.h.NetStim()
+    ns.interval = 100000
+    ns.number = 1
+    ns.start = SYN_ACTIVATION_T
+    ns.noise = 0
+    nc= bglibpy.neuron.h.NetCon(ns,syn,0,SYN_DELAY,SYN_G)
+
+    sim = bglibpy.Simulation()
+    sim.addCell(cell)
+    sim.run(T_STOP,v_init=V_INIT,cvode=False,dt=DT)
+    werner_t = cell.getTime()
+    werner_v = cell.getSomaVoltage()
+    del(sim)
+    del(syn)
+    del(ns)
+    del(nc)
+    return werner_t,werner_v
     
-# hines_t,hines_v = run_hines_bs(soma_L,soma_D)
-# plt.plot(hines_t,hines_v,label='hines, direct')
+def test_bs_expsyn_pyneuron_vs_bglibpy(graph=False) :
+    global d_derived, l_derived
+    '''
+    The real stuff, part I
+    Run the ball-and-stick model by 1. PyNEURON, 2. bglibpy, 3. analytic
+    '''
+    compute_some_settings_from_ASC_file()    
+    ''' Run the Golden Standard: PyNEURON '''
+    d_derived = 10
+    l_derived = soma_A / (2*np.pi*d_derived/2.0)
+    print 'soma_A=%f, derived D=%f, L=%f -> A=%f' % \
+      (soma_A,d_derived,l_derived,surface(d_derived/2.0,l_derived))
+    hines_t,hines_v = run_hines_bs(l_derived,d_derived)
 
-d_derived = 10
-l_derived = soma_A / (2*np.pi*d_derived/2.0)
-print 'soma_A=%f, derived D=%f, L=%f -> A=%f' % (soma_A,d_derived,l_derived,surface(d_derived/2.0,l_derived))
-hines_t,hines_v = run_hines_bs(l_derived,d_derived)
-plt.plot(hines_t,hines_v,label='hines, derived')
+    ''' Run with bglibpy (Werner) '''
+    werner_t, werner_v = run_bglib_bs()    
+    numpy.testing.assert_array_almost_equal(hines_v,werner_v,1,err_msg=\
+                                            'Werner != Hines')
 
-''' 3: Pseudo/Semi-Analytical (Willem)'''
+    if(graph) :
+        plt.plot(werner_t,werner_v,'g',label='werner')
+        plt.plot(hines_t,hines_v,'b',label='hines, derived')
+        plt.legend(loc=0)
+        plt.show() 
+        
+def test_bs_expsyn_pyneuron_vs_analytic(graph=False) :
+    global d_derived, l_derived
+    '''
+    The real stuff, part I
+    Run the ball-and-stick model by 1. PyNEURON, 2. bglibpy, 3. analytic
+    '''
+    compute_some_settings_from_ASC_file()    
+    ''' Run the Golden Standard: PyNEURON '''
+    d_derived = 10
+    l_derived = soma_A / (2*np.pi*d_derived/2.0)
+    print 'soma_A=%f, derived D=%f, L=%f -> A=%f' % \
+      (soma_A,d_derived,l_derived,surface(d_derived/2.0,l_derived))
+    hines_t,hines_v = run_hines_bs(l_derived,d_derived)
 
-''' write config file for Willem '''
-f_name = 'bs.cfg'
-outF = open(f_name,'w')
-outF.write('[neuron]\n')
-outF.write('CM: %f\n' % (CM))
-outF.write('RM: %f\n' % (RM))
-outF.write('RA: %f\n' % (RA))
-outF.write('EL: %f\n\n' % (EL))
-outF.write('[soma]\n')
-outF.write('D: ' + str(d_derived)+'\n')
-outF.write('L: ' + str(l_derived)+'\n\n')
-outF.write('[morph]\n')
-outF.write('lengths: [%f]\n' % (dend0_L))
-outF.write('diams: [%f]\n' % (dend0_D))
-outF.close()
+    ''' Compute the Pseudo/Semi-Analytical solution (Willem)'''
+    t_willem, v_willem = run_analytic(dt=DT)
 
-print 'going to do the analytical stuff'
-v_willem,t_willem = compute_system([[SYN_ACTIVATION_T]],T_STOP,DT,\
-                                   conffile_name=f_name,numsyn=1,syndend=[0],\
-                                   synloc=[SYN_LOC],gbar=[SYN_G],\
-                                   decay=[SYN_DECAY],E_rev=[SYN_E])
-plt.plot(t_willem,v_willem,label='willem, DT=%f' % (DT))
+    numpy.testing.assert_array_almost_equal(hines_v,v_willem,1,err_msg=\
+                                            'Willem != Hines')                                            
 
-''' 2: bglibpy.Cell (Werner) '''
-syn = bglibpy.neuron.h.ExpSyn(SYN_LOC,sec=cell.basal[0])
-syn.tau = SYN_DECAY
-syn.e = SYN_E
-ns = bglibpy.neuron.h.NetStim()
-ns.interval = 100000
-ns.number = 1
-ns.start = SYN_ACTIVATION_T
-ns.noise = 0
-nc= bglibpy.neuron.h.NetCon(ns,syn,0,SYN_DELAY,SYN_G)
+    if(graph) :
+        plt.plot(t_willem,v_willem,'r',label='willem, DT=%f' % (DT))
+        plt.plot(hines_t,hines_v,'b',label='hines, derived')
+        plt.legend(loc=0)
+        plt.show() 
 
-sim = bglibpy.Simulation()
-sim.addCell(cell)
-sim.run(T_STOP,v_init=V_INIT,cvode=False,dt=DT)
-werner_t = cell.getTime()
-werner_v = cell.getSomaVoltage()
-plt.plot(werner_t,werner_v,label='werner')
-del(sim)
-del(syn)
-del(ns)
-del(nc)
+def test_bs_ProbAMPANMDAEMS_pyneuron_vs_bglibpy() :
+    ''' pff. I'm done with nice encapsulation. just running a test!'''
+    cell = bglibpy.Cell("ballstick.hoc", "./")    
+    syn = bglibpy.neuron.h.ProbAMPANMDA_EMS(SYN_LOC,sec=cell.basal[0])
+    syn.tau_r_AMPA = 0.1
+    syn.tau_r_NMDA = 0.1
+    syn.tau_d_AMPA = SYN_DECAY
+    syn.tau_d_NMDA = SYN_DECAY
+    syn.NMDA_ratio = 0
+    syn.Use = 1.0
+    ns = bglibpy.neuron.h.NetStim()
+    ns.interval = 100000
+    ns.number = 1
+    ns.start = SYN_ACTIVATION_T
+    ns.noise = 0
+    nc= bglibpy.neuron.h.NetCon(ns,syn,0,SYN_DELAY,SYN_G)
 
-syn = bglibpy.neuron.h.ProbAMPANMDA_EMS(SYN_LOC,sec=cell.basal[0])
-syn.tau_r_AMPA = 0.1
-syn.tau_r_NMDA = 0.1
-syn.tau_d_AMPA = SYN_DECAY
-syn.tau_d_NMDA = SYN_DECAY
-syn.NMDA_ratio = 0
-syn.Use = 1.0
-ns = bglibpy.neuron.h.NetStim()
-ns.interval = 100000
-ns.number = 1
-ns.start = SYN_ACTIVATION_T
-ns.noise = 0
-nc= bglibpy.neuron.h.NetCon(ns,syn,0,SYN_DELAY,SYN_G)
+    sim = bglibpy.Simulation()
+    sim.addCell(cell)
+    sim.run(T_STOP,v_init=V_INIT,cvode=False,dt=DT)
+    werner_t = cell.getTime()
+    werner_v = cell.getSomaVoltage()
 
-# sim = bglibpy.Simulation()
-# sim.addCell(cell)
-# sim.run(T_STOP,v_init=V_INIT,cvode=False,dt=DT)
-# werner_t = cell.getTime()
-# werner_v = cell.getSomaVoltage()
-# plt.plot(werner_t,werner_v,'+',label='werner2')
+    del(sim)
+    del(ns);del(nc);del(syn)
+    del(cell)
 
-''' Eilif's BGLIB'''
-s = bluepy.Simulation("BlueConfig")
-# excitatory cell with an incoming inhibitory synapse 
-#v_a1 = s.reports.soma.time_series(1)
-# inhibitory cell with an incoming excitatory synapse 
-v_a2 = s.reports.soma.time_series(2)
-t = s.reports.soma.time_range- 401 + SYN_ACTIVATION_T
-plt.plot(t,v_a2,label='Eilif')
+    assert False == True
 
-plt.legend(loc=0)
-plt.show()
+def test_bs_ProbAMPANMDAEMS_pyneuron_vs_bglib() :
+    assert False == True
+
+# ''' Eilif's BGLIB'''
+# s = bluepy.Simulation("BlueConfig")
+# # excitatory cell with an incoming inhibitory synapse 
+# #v_a1 = s.reports.soma.time_series(1)
+# # inhibitory cell with an incoming excitatory synapse 
+# v_a2 = s.reports.soma.time_series(2)
+# t = s.reports.soma.time_range- 401 + SYN_ACTIVATION_T
+# plt.plot(t,v_a2,label='Eilif')
