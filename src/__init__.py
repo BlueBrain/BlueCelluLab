05e3c19e382d2696946e52cefdd7b8a6c7b11af0
Werner Van Geit <werner.vangeit@epfl.ch>
2012-12-17 14:06:36 +0100

    Resolved an import warning in __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,12 @@
 """
 Python library for running single cell bglib templates
 """
-from cell import Cell
-from plotwindow import PlotWindow
-from dendrogram import Dendrogram
-from psection import PSection
-from psegment import PSegment
-from simulation import Simulation
-from importer import *
-from tools import *
-from ssim import SSim
+from .cell import Cell
+from .plotwindow import PlotWindow
+from .dendrogram import Dendrogram
+from .psection import PSection
+from .psegment import PSegment
+from .simulation import Simulation
+from .importer import *
+from .tools import *
+from .ssim import SSim
