e3f9d53b347826bfad77a9faa0be81345048b570
Werner Van Geit <werner.vangeit@epfl.ch>
2013-05-31 14:15:51 +0200

    Replaced assert_equal with assert_almost_equal for tapering test

--- test/test_bglib.py
+++ test/test_bglib.py
@@ -24,22 +24,21 @@ class TestBGLibBaseClass1(object):
     def test_tapering(self):
         """Test if tapering in Cell.hoc works correctly"""
         axon0_taper_diams = [segment.diam for segment in self.taper_cell.axonal[0]]
-        nt.assert_equal(axon0_taper_diams, [1.9, 1.7, 1.5, 1.3, 1.1])
+        nt.assert_almost_equal(axon0_taper_diams, [1.9, 1.7, 1.5, 1.3, 1.1])
 
         axon1_taper_diams = [segment.diam for segment in self.taper_cell.axonal[1]]
-        nt.assert_equal(axon1_taper_diams,
+        nt.assert_almost_equal(axon1_taper_diams,
                 [0.9642857142857143, 0.8928571428571429,
                     0.8214285714285714, 0.75,
                     0.6785714285714286, 0.6071428571428571,
                     0.5357142857142857])
 
         myelin_taper_diams = [segment.diam for segment in self.taper_cell.cell.getCell().myelin[0]]
-        nt.assert_equal(myelin_taper_diams, [0.25]*21)
-
+        nt.assert_almost_equal(myelin_taper_diams, [0.25]*21)
 
         axon0_notaper_diams = [segment.diam for segment in self.notaper_cell.axonal[0]]
-        nt.assert_equal(axon0_notaper_diams, [0.7484678706370597])
+        nt.assert_almost_equal(axon0_notaper_diams, [0.7484678706370597])
 
         axon1_notaper_diams = [segment.diam for segment in self.notaper_cell.axonal[1]]
-        nt.assert_equal(axon1_notaper_diams, [0.14187096846614167])
+        nt.assert_almost_equal(axon1_notaper_diams, [0.14187096846614167])
 
