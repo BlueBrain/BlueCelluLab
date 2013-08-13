a338b7d527da7de580e58ad92607b1a6a155eeaa
Werner Van Geit <werner.vangeit@epfl.ch>
2013-08-13 17:06:05 +0200

    Added progress bar to Simulation

--- src/simulation.py
+++ src/simulation.py
@@ -9,37 +9,59 @@
 """
 
 import sys
+import bglibpy
 from bglibpy.importer import neuron
 from bglibpy import printv
 from bglibpy import printv_err
 from bglibpy import tools
 
+
 class Simulation(object):
 
     """Class that represents a neuron simulation"""
-    def __init__(self, show_progress=False):
+    def __init__(self):
         self.cells = []
-        self.fih = None
-        if show_progress:
-            self.progress = neuron.h.ShowProgress( neuron.h.cvode, 0 )
-        else:
-            self.progress = None
+        self.fih_progress = None
 
     def add_cell(self, new_cell):
         """Add a cell to a simulation"""
         self.cells.append(new_cell)
 
+    def init_progress_callback(self):
+        self.progress = 0
+        if not self.fih_progress:
+            self.fih_progress = neuron.h.FInitializeHandler(
+                1, self.progress_callback)
 
+    def progress_callback(self):
+        self.progress += 1
+        sys.stdout.write("\r[%s%s]" % ("#" * self.progress, " " * (100 - self.progress)))
+        sys.stdout.flush()
+
+        neuron.h.cvode.event(
+            neuron.h.t + self.progress_dt, self.progress_callback)
 
     def init_callbacks(self):
         """Initialize the callback of all the registered simulation objects (e.g. for window plotting)"""
         for cell in self.cells:
             cell.init_callbacks()
-        #if not self.progress and bglibpy.VERBOSE_LEVEL > 0:
-        #    self.progress = neuron.h.ShowProgress( neuron.h.cvode, 0 )
 
-    def run(self, maxtime, cvode=True, celsius=34, v_init=-65, dt=0.025, forward_skip=None):
+    def run(self, maxtime, cvode=True, celsius=34, v_init=-65, dt=0.025, forward_skip=None, show_progress=None):
         """Run the simulation"""
+        if maxtime <= neuron.h.t:
+            raise Exception("Simulation: need to provide a maxtime (=%f) that is bigger than the current time (=%f)" % (
+                maxtime, neuron.h.t))
+
+        if show_progress == None:
+            if bglibpy.VERBOSE_LEVEL > 1:
+                show_progress = True
+            else:
+                show_progress = False
+
+        if show_progress:
+            self.progress_dt = (maxtime - neuron.h.t)/100
+            self.init_progress_callback()
+
         neuron.h.celsius = celsius
         neuron.h.tstop = maxtime
 
@@ -48,7 +70,8 @@ class Simulation(object):
             neuron.h.cvode_active(1)
         else:
             if cvode_old_status:
-                printv("WARNING: cvode was activated outside of Simulation, temporarily disabling it in run() because cvode=False was set", 2)
+                printv(
+                    "WARNING: cvode was activated outside of Simulation, temporarily disabling it in run() because cvode=False was set", 2)
             neuron.h.cvode_active(0)
 
         neuron.h.v_init = v_init
@@ -84,15 +107,16 @@ class Simulation(object):
             neuron.h.dt = save_dt
             neuron.h.t = 0
 
-
-        #pylint: disable=W0703
+        # pylint: disable=W0703
         try:
             neuron.h.continuerun(neuron.h.tstop)
-        except Exception, e:
-            printv_err('The neuron was eaten by the Python !\nReason: %s: %s' % (e.__class__.__name__, e), 1)
+        except Exception as e:
+            printv_err('The neuron was eaten by the Python !\nReason: %s: %s' % (
+                e.__class__.__name__, e), 1)
         finally:
             if cvode_old_status:
-                printv("WARNING: cvode was activated outside of Simulation, this might make it impossible to load templates with stochastic channels", 2)
+                printv(
+                    "WARNING: cvode was activated outside of Simulation, this might make it impossible to load templates with stochastic channels", 2)
             neuron.h.cvode_active(cvode_old_status)
 
         printv('Finished simulation', 1)
@@ -104,4 +128,3 @@ class Simulation(object):
     def addCell(self, new_cell):
         """Add a cell to a simulation"""
         self.add_cell(new_cell)
-
