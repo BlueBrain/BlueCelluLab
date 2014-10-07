ff03518c56ebb64a9bfe0201c86039673b40c817
Werner Van Geit <werner.vangeit@epfl.ch>
2014-10-07 16:39:19 +0200

    Added function to find the euclidian distance between two sections in a morphology

--- bglibpy/bglibpy/cell.py
+++ bglibpy/bglibpy/cell.py
@@ -746,6 +746,95 @@ class Cell(object):
                         "somaticbranches: No apic or \
                                 dend found in section %s" % secname)
 
+    @staticmethod
+    def grindaway(hsection):
+        """Grindaway"""
+
+        # get the data for the section
+        n_segments = neuron.h.n3d(sec=hsection)
+        n_comps = hsection.nseg
+
+        xs = numpy.array(n_segments)
+        ys = numpy.array(n_segments)
+        zs = numpy.array(n_segments)
+        lengths = numpy.array(n_segments)
+        for index in range(0, n_segments):
+            xs[index] = neuron.h.x3d(index)
+            ys[index] = neuron.h.y3d(index)
+            zs[index] = neuron.h.z3d(index)
+            lengths[index] = neuron.h.arc3d(index)
+
+        # to use Vector class's .interpolate()
+        # must first scale the independent variable
+        # i.e. normalize length along centroid
+        lengths /= (lengths[-1])
+
+        # initialize the destination "independent" vector
+        # range = numpy.array(n_comps+2)
+        comp_range = numpy.arange(0, n_comps + 2) / n_comps - 1 / (2 * n_comps)
+        comp_range[0] = 0
+        comp_range[-1] = 1
+
+        # length contains the normalized distances of the pt3d points
+        # along the centroid of the section.  These are spaced at
+        # irregular intervals.
+        # range contains the normalized distances of the nodes along the
+        # centroid of the section.  These are spaced at regular intervals.
+        # Ready to interpolate.
+
+        xs_interp = numpy.interp(comp_range, lengths, xs)
+        ys_interp = numpy.interp(comp_range, lengths, ys)
+        zs_interp = numpy.interp(comp_range, lengths, zs)
+
+        return xs_interp, ys_interp, zs_interp
+
+    @staticmethod
+    def euclid_section_distance(
+            hsection1=None,
+            hsection2=None,
+            location1=None,
+            location2=None,
+            projection=None):
+        """Calculate euclidian distance between positions on two sections
+
+        Parameters
+        ----------
+
+        hsection1 : hoc section
+                    First section
+        hsection2 : hoc section
+                    Second section
+        location1 : float
+                    range x along hsection1
+        location2 : float
+                    range x along hsection2
+        projection : string
+                     planes to project on, e.g. 'xy'
+        """
+
+        xs_interp1, ys_interp1, zs_interp1 = Cell.grindaway(hsection1)
+        xs_interp2, ys_interp2, zs_interp2 = Cell.grindaway(hsection2)
+
+        x1 = xs_interp1[int(numpy.floor(len(xs_interp1) * location1))]
+        y1 = ys_interp1[int(numpy.floor(len(ys_interp1) * location1))]
+        z1 = zs_interp1[int(numpy.floor(len(zs_interp1) * location1))]
+
+        x2 = xs_interp2[int(numpy.floor(len(xs_interp2) * location2))]
+        y2 = ys_interp2[int(numpy.floor(len(ys_interp2) * location2))]
+        z2 = zs_interp2[int(numpy.floor(len(zs_interp2) * location2))]
+
+        distance = 0
+        if 'x' in projection:
+            distance += (x1 - x2) ** 2
+        if 'y' in projection:
+            distance += (y1 - y2) ** 2
+        if 'z' in projection:
+            distance += (z1 - z2) ** 2
+
+        distance = numpy.sqrt(distance)
+
+        return distance
+
     def apical_trunk(self):
         """Return the apical trunk of the cell."""
         if len(self.apical) is 0:
