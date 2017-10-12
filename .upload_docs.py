ef45d0e964e66ad5d1d3f5b70083f0f97bc71df9
Werner Van Geit <werner.vangeit@epfl.ch>
2017-10-12 10:32:09 +0200

    Store all major.minor versions on doc server

--- .upload_docs.py
+++ .upload_docs.py
@@ -7,7 +7,6 @@ import os
 import contextlib
 import datetime
 import shutil
-import glob
 
 import sh
 
@@ -54,6 +53,7 @@ def main():
     with cd('jekylltest'):
         print('Pulling latest jekylltest ...')
         sh.git('reset', '--hard', 'origin/master')
+        sh.git('clean', '-fd')
         sh.git('pull')
 
         import bglibpy
@@ -61,17 +61,31 @@ def main():
         bglibpy_major_version, bglibpy_minor_version, _ = \
             bglibpy.__version__.split('.')
 
-        doc_subdir = "BGLibPy-%s" % bglibpy_version
+        doc_subdir = "BGLibPy-%d.%d" % (int(bglibpy_major_version),
+                                        int(bglibpy_minor_version))
+
+        finished_filename = os.path.join(doc_subdir, '.doc_version')
+        metadata_filename = os.path.join('_projects', doc_subdir)
+
+        bglibpy_prev_version = None
+
+        if os.path.exists(finished_filename):
+            with open(finished_filename) as finished_file:
+                bglibpy_prev_version = finished_file.read()
+
+        print("Previous BGLibPy version: %s" % bglibpy_version)
 
         print('Doc subdir: %s' % doc_subdir)
 
-        if not os.path.exists(doc_subdir):
-            print('Removing old docs ...')
-            for old_dir in glob.glob('BGLibPy-*'):
-                sh.git('rm', '-r', old_dir)
+        if bglibpy_prev_version is None or \
+                bglibpy_prev_version != bglibpy_version:
 
-            for old_metadata in glob.glob('_projects/BGLibPy-*'):
-                sh.git('rm', old_metadata)
+            print('Removing old docs ...')
+
+            if os.path.exists(doc_subdir):
+                sh.git('rm', '-r', doc_subdir)
+            if os.path.exists(metadata_filename):
+                sh.git('rm', metadata_filename)
 
             print('Copying %s to %s ...' % (doc_dir, doc_subdir))
             shutil.copytree(doc_dir, doc_subdir)
@@ -84,15 +98,19 @@ def main():
 
             print('Created metadata: %s' % metadata_content)
 
-            metadata_filename = os.path.join('_projects', doc_subdir)
-
             with open(metadata_filename, 'w') as metadata_file:
                 metadata_file.write(metadata_content)
 
             print('Wrote metadata to: %s' % metadata_filename)
 
+            with open(finished_filename, 'w') as finished_file:
+                finished_file.write(bglibpy_prev_version)
+
+            print('Wrote doc version info to: %s' % finished_filename)
+
             sh.git('add', metadata_filename)
             sh.git('add', doc_subdir)
+            sh.git('add', finished_filename)
 
             print('Added doc to repo')
 
