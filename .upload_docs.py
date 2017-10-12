8879a422ed2635723447ba486ef6f1eac5e2dbc4
Werner Van Geit <werner.vangeit@epfl.ch>
2017-10-12 11:12:57 +0200

    Small fixes in doc_upload

--- .upload_docs.py
+++ .upload_docs.py
@@ -51,21 +51,29 @@ def main():
         sh.git('clone', '-b', 'master', '--depth=1', doc_repo)
 
     with cd('jekylltest'):
-        print('Pulling latest jekylltest ...')
-        sh.git('reset', '--hard', 'origin/master')
-        sh.git('clean', '-fd')
-        sh.git('pull')
 
+        print('Reading BGLibPy version ...')
         import bglibpy
         bglibpy_version = bglibpy.__version__
         bglibpy_major_version, bglibpy_minor_version, _ = \
             bglibpy.__version__.split('.')
+        print('BGLibPy version is: %s' % bglibpy_version)
 
         doc_subdir = "BGLibPy-%d.%d" % (int(bglibpy_major_version),
                                         int(bglibpy_minor_version))
 
+        print('Doc subdir: %s' % doc_subdir)
+
         finished_filename = os.path.join(doc_subdir, '.doc_version')
+
+        if os.path.exists(finished_filename):
+            os.remove(finished_filename)
+
         metadata_filename = os.path.join('_projects', doc_subdir)
+
+        print('Pulling latest jekylltest ...')
+        sh.git('reset', '--hard', 'origin/master')
+        sh.git('pull')
 
         bglibpy_prev_version = None
 
@@ -73,19 +81,15 @@ def main():
             with open(finished_filename) as finished_file:
                 bglibpy_prev_version = finished_file.read()
 
-        print("Previous BGLibPy version: %s" % bglibpy_version)
-
-        print('Doc subdir: %s' % doc_subdir)
+        print("Previous BGLibPy version: %s" % bglibpy_prev_version)
 
         if bglibpy_prev_version is None or \
                 bglibpy_prev_version != bglibpy_version:
 
             print('Removing old docs ...')
 
-            if os.path.exists(doc_subdir):
-                sh.git('rm', '-r', doc_subdir)
-            if os.path.exists(metadata_filename):
-                sh.git('rm', metadata_filename)
+            sh.git('rm', '-r', '--ignore-unmatch', doc_subdir)
+            sh.git('rm', '--ignore-unmatch', metadata_filename)
 
             print('Copying %s to %s ...' % (doc_dir, doc_subdir))
             shutil.copytree(doc_dir, doc_subdir)
@@ -104,7 +108,7 @@ def main():
             print('Wrote metadata to: %s' % metadata_filename)
 
             with open(finished_filename, 'w') as finished_file:
-                finished_file.write(bglibpy_prev_version)
+                finished_file.write(bglibpy_version)
 
             print('Wrote doc version info to: %s' % finished_filename)
 
