38489e6b2ecd0b91b9883e6fd1ada663ac241c7c
Werner Van Geit <werner.vangeit@epfl.ch>
2017-10-13 13:55:30 +0200

    Add jenkins shell script

---
+++ .jenkins.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+set -x
+
+tox_args='-e py27-test'
+
+if [ "${os}" = "cscsviz" ]
+then
+	. /opt/rh/python27/enable
+    tox_args="${tox_args}-v5"
+elif [ "${os}" = "Ubuntu-16.04" ]
+then
+	tox_args="${tox_args}-upload_docs-devpi"
+fi
+
+which python
+python --version
+
+cd $WORKSPACE
+
+#########
+# Virtualenv
+#########
+
+if [ ! -d "${WORKSPACE}/env" ]; then
+  virtualenv ${WORKSPACE}/env --no-site-packages
+fi
+
+. ${WORKSPACE}/env/bin/activate
+pip install pip --upgrade
+pip install tox --upgrade 
+
+#####
+# Tests
+#####
+
+cd  ${WORKSPACE}/BGLibPy
+
+tox ${tox_args}
