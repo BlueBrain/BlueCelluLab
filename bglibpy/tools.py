be43f0a7b7643e13fa4097cba5ecda945fc37ae9
Werner Van Geit <werner.vangeit@epfl.ch>
2019-06-05 12:31:22 +0200

    Extend numpy encoder for json in python3
    Change-Id: I7ca03e57ace7f6703aa6c3bd81e68060448c03fd

--- bglibpy/tools.py
+++ bglibpy/tools.py
@@ -659,6 +659,13 @@ def search_hyp_current_replay_imap(blueconfig, gid_list, timeout=600,
 
 class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
-        if isinstance(obj, numpy.ndarray):
+        if isinstance(obj, (numpy.int_, numpy.intc, numpy.intp, numpy.int8,
+                            numpy.int16, numpy.int32, numpy.int64, numpy.uint8,
+                            numpy.uint16, numpy.uint32, numpy.uint64)):
+            return int(obj)
+        elif isinstance(obj, (numpy.float_, numpy.float16, numpy.float32,
+                              numpy.float64)):
+            return float(obj)
+        elif isinstance(obj, numpy.ndarray):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
