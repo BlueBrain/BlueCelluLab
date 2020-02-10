e3cf4a1be44aaf0b73b6a8a71f30ff4e0df7f9e7
Tuncel Anil <anil.tuncel@epfl.ch>
2020-02-10 13:20:32 +0100

    removed unnecessary cp operations from install_neurodamus
    Change-Id: Ia43ed2ce7deffeb2e5fd7d3b6173c54736581d42

--- .install_neurodamus.sh
+++ .install_neurodamus.sh
@@ -17,13 +17,6 @@ then
     echo "Downloading neocortex channels ..."
     rm -rf neocortex
     git clone --depth 1 --recursive ssh://bbpcode.epfl.ch/sim/models/neocortex
- 
-    # Remove the 4 lines below once these mod files are part of the channel repos
-    cp neurodamus-core/mod/netstim_inhpoisson.mod neocortex/mod/v5
-    cp neurodamus-core/mod/VecStim.mod neocortex/mod/v5
-    
-    cp neurodamus-core/mod/netstim_inhpoisson.mod neocortex/mod/v6
-    cp neurodamus-core/mod/VecStim.mod neocortex/mod/v6
 
     # Remove this line once we have a canonical v6 sim
     cp neocortex/mod/v5/Ca_HVA.mod neocortex/mod/v6
