087afd11c8fcb14733b16d519c3c6720fe800c7b
Werner Van Geit <werner.vangeit@epfl.ch>
2020-02-12 16:55:51 +0100

    Trying build with other nrn commit
    Change-Id: I2f1de27d3ac5cfea7a38ff9d490b643d36ce7fc4

--- .install_neuron.sh
+++ .install_neuron.sh
@@ -18,8 +18,8 @@ then
     if [ ! -d nrn ]                                               
     then
         echo "Downloading NEURON from github ..."
-        git clone --depth 100 https://github.com/nrnhines/nrn.git >download.log 2>&1
-        git checkout 73236b12977118ae0a98d7dbbed60973994cdaee
+        git clone https://github.com/nrnhines/nrn.git >download.log 2>&1
+        git checkout 3c1384d6af80cb22d693be1262312d3f9a9acc9c
     else                                                                         
         echo "Neuron already downloaded"
     fi
