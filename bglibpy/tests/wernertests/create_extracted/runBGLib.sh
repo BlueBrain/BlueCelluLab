5a4474f9d0940557b0bdc3eb1e4eaac0e78b7016
Anil Tuncel <anil.tuncel@epfl.ch>
2020-07-03 15:02:26 +0200

    removed unused tests depending on the data that no longer exist
    These tests used to depend on the data stored at /bgscratch
    
    Change-Id: I8c2392d23b7308c5091eb1a4fe743a6cddb37b0e

--- bglibpy/tests/wernertests/create_extracted/runBGLib.sh
+++
@@ -1,7 +0,0 @@
-module load $HOME/rhel6-mvapich2-psm-x86_64-shared-dev 
-#mpirun -n 2 -genvall $BGLIB_ROOT/bin/special $BGLIB_HOCLIB/PrepConfig.hoc -c 'configFile="BlueConfig"' $BGLIB_HOCLIB/init.hoc -mpi -NFRAME 256
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/nfs4/bbp.epfl.ch/sw/bbp-stack/reportinglib/dev-git/install/lib
-cd $1
-mpirun -n 2 -genvall /home/vangeit/src/bbp/lib/x86_64/special  $BGLIB_HOCLIB/PrepConfig.hoc -c "configFile=\"BlueConfig\"" $BGLIB_HOCLIB/init.hoc -mpi -NFRAME 256
-cd output
-python /home/ebmuller/src/bbp-user-ebmuller/pybinreports/soma2h5.py
