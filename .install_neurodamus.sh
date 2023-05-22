0d63d52c21db4ddca1f9eccd9e200bd1f1a80cd7
Anil Tuncel <anil.tuncel@epfl.ch>
2023-05-22 13:39:36 +0200

    use add local mechanisms to be used in tests

--- .install_neurodamus.sh
+++ .install_neurodamus.sh
@@ -13,14 +13,7 @@ then
     echo "Downloading neurodamus core ..." 
     rm -rf neurodamus-core
     git clone -q --depth 1 git@bbpgitlab.epfl.ch:hpc/sim/neurodamus-core.git
-    
-    echo "Downloading neocortex channels ..."
-    rm -rf neocortex
-    git clone -q --depth 1 --recursive git@bbpgitlab.epfl.ch:hpc/sim/models/neocortex.git
 
-    # Remove this line once we have a canonical v6 sim
-    cp neocortex/mod/v5/Ca_HVA.mod neocortex/mod/v6
-    
     touch -f .install_finished
     echo "Neurodamus successfully installed"
 else
