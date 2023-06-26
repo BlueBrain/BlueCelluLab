7bb370d3e5632e899a0784c4f4aece7870f5b4fd
Anil Tuncel <tuncel.manil@gmail.com>
2023-06-26 14:15:26 +0200

    rename bluecellulab.cell.cell.Cell -> bluecellulab.cell.core.Cell

--- bluecellulab/cell/__init__.py
+++ bluecellulab/cell/__init__.py
@@ -1,5 +1,5 @@
 """The cell module representing the NEURON cell and operations on it."""
 
-from .cell import Cell
+from .core import Cell
 from .cell_dict import CellDict
 from .sonata_proxy import SonataProxy
