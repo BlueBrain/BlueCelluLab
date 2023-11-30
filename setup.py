b5ed5e0a50dae31ca84c1deccf310071807d17da
Anil Tuncel <anil.tuncel@epfl.ch>
2023-11-30 10:02:26 +0100

    use bluepysnap<3.0.0

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setuptools.setup(
         "numpy>=1.8.0,<2.0.0",
         "matplotlib>=3.0.0,<4.0.0",
         "pandas>=1.0.0,<3.0.0",
-        "bluepysnap>=1.0.5,<2.0.0",
+        "bluepysnap>=1.0.5,<3.0.0",
         "pydantic>=1.10.2,<2.0.0",
         "typing-extensions>=4.8.0"
     ],
