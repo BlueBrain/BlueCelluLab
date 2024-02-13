f443644fbedb5d28eb8bd3c0a186e7c849eb5b99
Anil Tuncel <anil.tuncel@epfl.ch>
2024-02-13 14:58:55 +0100

    avoid accessing private hoc attribute in core.py

--- bluecellulab/cell/core.py
+++ bluecellulab/cell/core.py
@@ -102,7 +102,7 @@ class Cell(InjectableMixin, PlottableMixin):
         self.cellname = neuron.h.secname(sec=self.soma).split(".")[0]
 
         # Set the gid of the cell
-        self.cell.getCell().gid = self.cell_id.id
+        public_hoc_cell(self.cell).gid = self.cell_id.id
 
         if rng_settings is None:
             self.rng_settings = RNGSettings("Random123")  # SONATA value
