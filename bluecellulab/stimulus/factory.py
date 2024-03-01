bb92e1bb372c4469d420168144a0e6e4d327772e
Anil Tuncel <anil.tuncel@epfl.ch>
2024-03-01 16:23:17 +0100

    add features to combine Stimulus objects

--- bluecellulab/stimulus/factory.py
+++ bluecellulab/stimulus/factory.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 class Stimulus(ABC):
-    def __init__(self, dt) -> None:
+    def __init__(self, dt: float) -> None:
         self.dt = dt
 
     @property
@@ -23,6 +23,10 @@ class Stimulus(ABC):
     def __len__(self) -> int:
         return len(self.time)
 
+    @property
+    def stimulus_time(self) -> float:
+        return len(self) * self.dt
+
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(dt={self.dt})"
 
@@ -38,12 +42,65 @@ class Stimulus(ABC):
     def plot_during_simulation(self, duration: float, ax=None, **kwargs):
         if ax is None:
             ax = plt.gca()
-        ax.plot(self.time, self.current, **kwargs)
+        # Create an array for the entire duration
+        full_time = np.arange(0, duration, self.dt)
+        full_current = np.zeros_like(full_time)
+
+        # Replace the corresponding values with self.time and self.current
+        indices = (self.time / self.dt).astype(int)
+        full_current[indices] = self.current
+
+        ax.plot(full_time, full_current, **kwargs)
         ax.set_xlabel("Time (ms)")
         ax.set_ylabel("Current (nA)")
         ax.set_title(self.__class__.__name__)
         ax.set_xlim(0, duration)
         return ax
+
+    def __add__(self, other: Stimulus) -> CombinedStimulus:
+        """Override + operator to concatenate Stimulus objects."""
+        if self.dt != other.dt:
+            raise ValueError("Stimulus objects must have the same dt to be concatenated")
+        # shift other time
+        other_time = other.time + self.time[-1] + self.dt
+        combined_time = np.concatenate([self.time, other_time])
+        # Concatenate the current arrays
+        combined_current = np.concatenate([self.current, other.current])
+        return CombinedStimulus(self.dt, combined_time, combined_current)
+
+
+class CombinedStimulus(Stimulus):
+    """Represents the Stimulus created by combining multiple stimuli."""
+    def __init__(self, dt: float, time: np.ndarray, current: np.ndarray) -> None:
+        super().__init__(dt)
+        self._time = time
+        self._current = current
+
+    @property
+    def time(self) -> np.ndarray:
+        return self._time
+
+    @property
+    def current(self) -> np.ndarray:
+        return self._current
+
+
+class EmptyStimulus(Stimulus):
+    """Represents empty stimulus (all zeros) that has no impact on the cell.
+
+    This is required by some Stimuli that expect the cell to rest.
+    """
+    def __init__(self, dt: float, duration: float) -> None:
+        super().__init__(dt)
+        self.duration = duration
+
+    @property
+    def time(self) -> np.ndarray:
+        return np.arange(0.0, self.duration, self.dt)
+
+    @property
+    def current(self) -> np.ndarray:
+        return np.zeros_like(self.time)
 
 
 class Step(Stimulus):
@@ -52,6 +109,20 @@ class Step(Stimulus):
         self.start = start
         self.end = end
         self.amplitude = amplitude
+
+    @classmethod
+    def as_ap_waveform(
+        cls,
+        dt: float,
+        threshold_current: float,
+        threshold_percentage: float,
+        start: float,
+        end: float,
+        post_wait: float,
+    ) -> CombinedStimulus:
+        amplitude = threshold_current * threshold_percentage / 100
+        res = cls(dt, start, end, amplitude) + EmptyStimulus(dt, duration=post_wait)
+        return res
 
     @property
     def time(self) -> np.ndarray:
@@ -97,3 +168,24 @@ class StimulusFactory:
         self, start: float, end: float, amplitude_start: float, amplitude_end: float
     ) -> Stimulus:
         return Ramp(self.dt, start, end, amplitude_start, amplitude_end)
+
+    def ap_waveform(
+        self,
+        threshold_current: float,
+        threshold_percentage: float = 220,
+        start: float = 250.0,
+        end: float = 300.0,
+        post_wait: float = 250.0,
+    ) -> Stimulus:
+        """Returns the APWaveform Stimulus object, a type of Step stimulus.
+
+        Args:
+            threshold_current: The threshold current of the Cell.
+            threshold_percentage: Percentage of desired threshold_current amplification.
+            start: The start time of the step.
+            end: The end time of the step.
+            post_wait: The time to wait after the end of the step.
+        """
+        return Step.as_ap_waveform(
+            self.dt, threshold_current, threshold_percentage, start, end, post_wait
+        )
--- tests/test_stimulus/test_factory.py
+++ tests/test_stimulus/test_factory.py
@@ -1,7 +1,7 @@
 import pytest
 import numpy as np
 
-from bluecellulab.stimulus.factory import Stimulus, Step, Ramp, StimulusFactory
+from bluecellulab.stimulus.factory import CombinedStimulus, EmptyStimulus, Stimulus, Step, Ramp, StimulusFactory
 
 
 class TestStimulus:
@@ -51,3 +51,42 @@ class TestStimulusFactory:
         assert isinstance(s, Ramp)
         assert np.all(s.time == np.arange(1, 2, self.dt))
         assert np.all(s.current == np.linspace(3, 4, 10))
+
+    def test_create_ap_waveform(self):
+        s = self.factory.ap_waveform(threshold_current=1)
+        assert isinstance(s, CombinedStimulus)
+        # below write np almost equal
+        np.testing.assert_allclose(s.time, np.arange(250, 550, self.dt))
+        assert s.current[0] == 2.2
+        assert s.current[-1] == 0.0
+
+
+def test_combined_stimulus():
+    """Test combining Stimulus objects."""
+    s1 = Step(0.1, 1, 2, 3)
+    s2 = Step(0.1, 3, 4, 5)
+    combined = s1 + s2
+
+    assert isinstance(combined, CombinedStimulus)
+    assert np.all(combined.time == np.concatenate([s1.time, s2.time + s1.time[-1] + 0.1]))
+    assert np.all(combined.current == np.concatenate([s1.current, s2.current]))
+    assert combined.dt == 0.1
+    assert len(combined) == len(s1) + len(s2)
+    assert combined.stimulus_time == (len(combined) * combined.dt)
+
+
+def test_combined_stimulus_different_dt():
+    s1 = Step(0.1, 1, 2, 3)
+    s2 = Step(0.2, 3, 4, 5)
+    with pytest.raises(ValueError):
+        combined = s1 + s2
+
+
+def test_empty_stimulus():
+    dt = 0.2
+    duration = 1.0
+    stimulus = EmptyStimulus(dt, duration)
+
+    assert stimulus.dt == dt
+    assert np.all(stimulus.time == np.arange(0, duration, dt))
+    assert np.all(stimulus.current == np.zeros_like(stimulus.time))
