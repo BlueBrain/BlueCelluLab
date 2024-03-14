e7b80881034aa4e6d16917d2288c22ba7ded5f1d
Anil Tuncel <anil.tuncel@epfl.ch>
2024-03-14 16:17:32 +0100

    allow IsolatedProcesses to run multiple processes

--- bluecellulab/utils.py
+++ bluecellulab/utils.py
@@ -50,5 +50,5 @@ class IsolatedProcess(Pool):
     Use this when running isolated NEURON simulations. Running 2 NEURON
     simulations on a single process is to be avoided.
     """
-    def __init__(self):
-        super().__init__(processes=1, maxtasksperchild=1)
+    def __init__(self, processes=1):
+        super().__init__(processes=processes, maxtasksperchild=1)
