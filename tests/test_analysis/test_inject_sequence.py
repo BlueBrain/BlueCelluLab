7e070033806a0be1c3bca64d57e0619050fc2077
Anil Tuncel <anil.tuncel@epfl.ch>
2024-03-15 18:48:05 +0100

    mock the multiprocessing in test_apply_multiple_step_stimuli

--- tests/test_analysis/test_inject_sequence.py
+++ tests/test_analysis/test_inject_sequence.py
@@ -1,4 +1,6 @@
 """Unit tests for the inject_sequence module."""
+from __future__ import annotations
+from unittest.mock import MagicMock, patch
 import pytest
 from bluecellulab import create_ball_stick
 from bluecellulab.analysis.inject_sequence import StimulusName, apply_multiple_step_stimuli, run_stimulus
@@ -15,19 +17,36 @@ def test_run_stimulus():
     assert len(recording.time) == len(recording.current)
 
 
-# this group is to prevent deadlock
-@pytest.mark.xdist_group(name="isolated-pool")
-def test_apply_multiple_step_stimuli():
-    """Test the apply_multiple_step_stimuli function."""
-    cell = create_ball_stick()
-    amplitudes = [80, 100, 120, 140]
-    recordings = apply_multiple_step_stimuli(cell, StimulusName.FIRE_PATTERN, amplitudes, duration=400, n_processes=4)
-    assert len(recordings) == len(amplitudes)
-    for recording in recordings.values():
-        assert len(recording.time) > 0
-        assert len(recording.time) == len(recording.voltage)
-        assert len(recording.time) == len(recording.current)
+class MockRecording:
+    def __init__(self):
+        self.time = [1, 2, 3]
+        self.voltage = [-70, -55, -40]
+        self.current = [0.1, 0.2, 0.3]
 
+
+@pytest.fixture
+def mock_run_stimulus():
+    return MagicMock(return_value=MockRecording())
+
+
+def test_apply_multiple_step_stimuli(mock_run_stimulus):
+    """Do not run the code in parallel, mock the return value via MockRecording."""
+    amplitudes = [80, 100, 120, 140]
+    cell = create_ball_stick()
+
+    with patch('bluecellulab.analysis.inject_sequence.IsolatedProcess') as mock_isolated_process, \
+         patch('bluecellulab.analysis.inject_sequence.run_stimulus', mock_run_stimulus):
+        # the mock process pool to return a list of MockRecordings
+        mock_isolated_process.return_value.__enter__.return_value.starmap.return_value = [MockRecording() for _ in amplitudes]
+
+        recordings = apply_multiple_step_stimuli(cell, StimulusName.FIRE_PATTERN, amplitudes, duration=400, n_processes=4)
+        assert len(recordings) == len(amplitudes)
+        for recording in recordings.values():
+            assert len(recording.time) > 0
+            assert len(recording.time) == len(recording.voltage)
+            assert len(recording.time) == len(recording.current)
+
+    # Testing unknown stimulus name
     with pytest.raises(ValueError) as exc_info:
         apply_multiple_step_stimuli(cell, "unknown", amplitudes, duration=400, n_processes=4)
     assert "Unknown stimulus name" in str(exc_info.value)
